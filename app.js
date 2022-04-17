baeb66b5d7357d1d9ead60940d5c7609c3fae077
muratgcmz <gocmezmurat@gmail.com>
2022-04-18 01:40:18 +0300

    v2

--- app.js
+++ app.js
@@ -54,14 +54,35 @@ const reviews = [
 
   // load intial item 
   window.addEventListener("DOMContentLoaded", function (){
-      const item = reviews[currentItem];
+      showPerson();
+  });
+
+  // show person based on item
+  function showPerson() {
+    const item = reviews[currentItem];
       img.src = item.img;
       author.textContent = item.name;
       job.textContent = item.job;
       info.textContent = item.text;
-  });
+  }
 
-  
+  // show next person
+
+  nextBtn.addEventListener("click" , function(){
+    currentItem++;
+    if(currentItem > reviews.length - 1)
+    currentItem = 0;
+    showPerson(currentItem)
+  })
+
+  // show prev person
+  prevBtn.addEventListener("click" , function(){
+    currentItem--;
+    if(currentItem  < 0){
+      currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+  })
 
 
   
